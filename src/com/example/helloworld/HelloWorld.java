93cf1ab5772e2a93b144f7ceeb83890e530a3bfa
nmaley <nastassia.maley@gmail.com>
2021-02-01 21:59:08 +0300

    second changes

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Java правит Интернетом!"); }
+        
         //System.out.println("Hello, World!"); }
       }
 
