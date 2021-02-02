305249c4df56921a94c81b41da5fa7971f5f9bf9
nmaley <nastassia.maley@gmail.com>
2021-02-02 20:51:22 +0300

    second commit

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,8 +2,7 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("Java правит Интернетом!"); }
-        
-        //System.out.println("Hello, World!"); }
-      }
+        System.out.println("Hello, World!");
+    }
+}
 
