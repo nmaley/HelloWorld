48d2b69f6ae3b60e1f23e337a7c829eaf16e959a
Nastassia Maleyeva <73353965+nmaley@users.noreply.github.com>
2021-02-02 21:05:34 +0300

    Update HelloWorld.java

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,8 +2,8 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String args[]) {
-
         System.out.println("Hello, World!");
+        System.out.println("2 февраля 2021 года");
     }
 }
 
